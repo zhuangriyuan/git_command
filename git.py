4671fe302fd91fd4f1473fa5178d96733af88b47
zhuangriyuan <zhuangriyuan@gmail.com>
2024-12-11 00:04:50 +0800

    final modify

--- git.py
+++ git.py
@@ -6,5 +6,6 @@
 # git commit -m "add new files"  # local commit
 # git log # 提交日志详细信息
 # git resset --hard commit_id   #回退版本，commit_id从git log中看
-# git remote add origin git@github.com:zhuangriyuan/git_command.git
+# git remote add origin git@github.com:zhuangriyuan/git_command.git #登录 GitHub，找到你的项目页面，点击右上角的绿色按钮 "Code"。复制仓库的 SSH 或 HTTPS 地址
+
 # git push -u origin main
