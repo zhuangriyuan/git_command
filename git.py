ebf09e527a91bb26a7c92260294f48f08eb22109
zhuangriyuan <zhuangriyuan@gmail.com>
2024-12-10 23:42:38 +0800

    add new command

--- git.py
+++ git.py
@@ -4,4 +4,7 @@
 # git add . # add all files
 # git status
 # git commit -m "add new files"  # local commit
-# git log # 提交日志
+# git log # 提交日志详细信息
+# git resset --hard commit_id   #回退版本，commit_id从git log中看
+# git remote add origin git@github.com:zhuangriyuan/git_command.git
+# git push -u origin main
