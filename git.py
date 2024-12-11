2b5ecd3c401c6a15b1ec4d58f2f1019735141885
zhuangriyuan <zhuangriyuan@gmail.com>
2024-12-11 15:20:34 +0800

    fix typo

--- git.py
+++ git.py
@@ -1,11 +1,12 @@
 # git clone
+
 # git init
 # git add filename
 # git add . # add all files
 # git status
 # git commit -m "add new files"  # local commit
-# git log # 提交日志详细信息
-# git resset --hard commit_id   #回退版本，commit_id从git log中看
 # git remote add origin git@github.com:zhuangriyuan/git_command.git #登录 GitHub，找到你的项目页面，点击右上角的绿色按钮 "Code"。复制仓库的 SSH 或 HTTPS 地址
-
 # git push -u origin main
+
+# git log # 提交日志详细信息
+# git reset --hard commit_id   #回退版本，commit_id从git log中看
